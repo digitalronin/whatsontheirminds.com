3969f620309409ec9bdeee79a4b7cce2b73b7737
Tom ten Thij <git@tomtenthij.nl>
2009-03-07 17:34:29 +0000

    Round font sizes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
 
   def cloud_tag(word)
-    style = "font-size: #{word.size_factor}em; left: #{word.x}px; top: #{word.y}px"
+    style = "font-size: #{(word.size_factor * 100).round / 100.0}em; left: #{word.x}px; top: #{word.y}px"
     %[<li class='word' style="#{style}">#{word.text} (#{word.value})</li>\n]
   end
 
