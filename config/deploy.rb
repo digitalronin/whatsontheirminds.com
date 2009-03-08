4a6188ebd84e873c2ec8e870fef05c92e1765d12
David Salgado <david@digitalronin.com>
2009-03-08 16:31:28 +0000

    setting up cap deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,19 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "parlour-tag"
+set :repository,  "git@github.com:digitalronin/parlour-tag.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :git_enable_submodules, 1
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+depend :remote, "#{deploy_to}/shared/system/keys.rb"
+
+role :app, "rewiredstate"
+role :web, "rewiredstate"
+role :db, "rewiredstate", :primary => true
+
