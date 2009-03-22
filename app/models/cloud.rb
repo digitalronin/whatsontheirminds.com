0dc568bba82374b104b2b1ea5b31248b3cd3b1da
David Salgado <david@digitalronin.com>
2009-03-22 23:33:22 +0000

    extracted term counting to separate, TextChunk class

--- app/models/cloud.rb
+++ app/models/cloud.rb
@@ -1,34 +1,14 @@
 class Cloud 
-  attr_accessor :title, :words, :max_count, :min_count  # need these to derive the right CSS class
+  attr_accessor :title, :words
 
   def initialize(params)
     @mp = params[:mp]
     @title = "#{@mp.full_name} Written Questions"
-    @words = count_words
+    tc = TextChunk.new text
+    @words = tc.terms_with_counts
   end
 
   private
-
-  def count_words
-    @max_count = 0
-    @min_count = 999
-
-    tc = TextChunk.new text
-
-    rtn = []
-    tc.terms.sort.each do |term|
-      value = tc.body.downcase.split(%r[#{term}]).size
-      RAILS_DEFAULT_LOGGER.debug "Counting: #{term}, #{value}"
-      @max_count = [ @max_count, value ].max
-      @min_count = [ @min_count, value ].min
-      word = Word.new(term, value)
-      rtn << word
-    end
-    # Return words in descending order of occurrences, so that the 
-    # biggest words are drawn on the page first, with smaller words
-    # on top, to make it easier to read the tag cloud
-    rtn.sort {|a,b| a.value <=> b.value}.reverse
-  end
 
   def text
     @text ||= @mp.written_answer_text
--- app/models/text_chunk.rb
+++ app/models/text_chunk.rb
@@ -27,4 +27,23 @@ class TextChunk
     list
   end
 
+  def terms_with_counts
+    @max_count = 0
+    @min_count = 999
+
+    rtn = []
+    terms.sort.each do |term|
+      value = body.downcase.split(%r[#{term}]).size
+      RAILS_DEFAULT_LOGGER.debug "Counting: #{term}, #{value}"
+      @max_count = [ @max_count, value ].max
+      @min_count = [ @min_count, value ].min
+      word = Word.new(term, value)
+      rtn << word
+    end
+    # Return words in descending order of occurrences, so that the 
+    # biggest words are drawn on the page first, with smaller words
+    # on top, to make it easier to read the tag cloud
+    rtn.sort {|a,b| a.value <=> b.value}.reverse
+  end
+
 end
